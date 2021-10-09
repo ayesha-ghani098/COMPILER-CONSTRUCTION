8ebdb24def75cc4e4740c54b83b952b39eb2319b
Hamza Arain <hamzaarain1999@gmail.com>
2021-10-09 02:32:29 -0700

    laxical code optimized

---
+++ Laxical_Analyzer/Essentials.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+
+namespace Laxical_Analyzer
+{
+    class Tools
+    {
+        public string readFile(string path) {
+            // Open the file to read from.
+            return File.ReadAllText(path);
+        }
+    }
+
+    public class Essentials
+    {
+        // Punctuators
+        // dictionary for punctuators and its classpart
+        public IDictionary<char, string> punctuators = new Dictionary<char, string>() {
+            {'(', "("}, {')',")"}, {'{', "{"}, {'}', "}"}, {'[', "["}, {']', "]"}, {';',";"}, {'.', "."}, {',', ","},
+            {'@', "lexical_error"}, {'$', "lexical_error"}, {'^', "lexical_error"}, {':', "lexical_error"}, {'?',"lexical_error"},
+            {'`', "lexical_error"}, {'~', "lexical_error"}, {'\'', "lexical_error"}, {'_', "lexical_error"}, {'\\', "lexical_error"},
+        };
+
+        // Operators
+        // dictionary for operators and its classpart
+        public IDictionary<string, string> operators = new Dictionary<string, string>() {
+            {"+", "PM"}, {"-", "PM"},
+            { "*", "MD"}, {"/", "MD"},
+            { "<", "RO"}, {">", "RO"}, {"<=", "RO"}, {">=", "RO"}, {"!=", "RO"}, {"==", "RO"},
+            {"=", "AO"}, {"+=", "AO"}, {"-=", "AO"},
+            {"&", "Lexeme error"}, {"|", "Lexeme error"},
+            {"!", "LO"}, {"||", "LO"}, {"&&", "LO"},
+        };
+    }
+}
